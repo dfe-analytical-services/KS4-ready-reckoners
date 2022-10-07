722d940fd3dccb83b6ac4e29079c63a715400d9e
FOSTER <Charlotte.FOSTER@education.gov.uk>
2022-10-07 11:54:11 +0100

    trying to add UI tests

--- tests/shinytest/initial_load_test.R
+++ tests/shinytest/initial_load_test.R
@@ -1,7 +1,35 @@
+run_set_shinytests <- function(dfinputs, outstring, listrecords) {
+  # This function loops through a set of inputs and takes a snapshot for each one.
+  # dfinputs: data frame containing field list and value list.
+  # outstring: the stem for the output filename.
+  # listrecords: list of input and output variables to record the values of.
+  for (i in 1:nrow(dfinputs)) {
+    file <- paste0(outstring, "_", i - 1, ".json")
+    cat(paste0(dfinputs$field[i], '="', dfinputs$value[i]), fill = TRUE)
+    eval(parse(text = paste0("app$setInputs(", dfinputs$field[i], '="', dfinputs$value[i], '", timeout_ = 1.2e+4, values_ = FALSE)')))
+    app$snapshot(
+      #      items = listrecords,
+      filename = file
+    )
+    #    clean_json(file)
+  }
+}
+
 app <- ShinyDriver$new("../../", loadTimeout = 6.e4)
 app$snapshotInit("initial_load_test", screenshot = FALSE)
+app$snapshot(
+  filename = "LoadHome.json"
+)
 
-app$snapshot(items = list(
-  input = c("bins"),
-  output = c("box_info")
-))
+
+dfTestInputs <- data.frame(
+  field = c(
+    "navlistPanel",
+    "tabsetpanels", "readinginput", "mathsinput", "p8scoreeng", "p8scoremath", "p8scoreebac", "p8scoreopen"
+  ),
+  value = c(
+    "Pupil ready reckoner",
+    "Progress 8", "120", "120", "18", "18", "27", "27"
+  )
+)
+run_set_shinytests(dfTestInputs, "KS4rRR", "")
