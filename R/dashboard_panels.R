f0583b9795d1ca9faa700002d1bcc07ef8c1ac15
Rich Bielby <richard.bielby@education.gov.uk>
2023-10-18 12:17:46 +0100

    Updated pupil tab panels to use bslib cards

--- R/dashboard_panels.R
+++ R/dashboard_panels.R
@@ -156,199 +156,193 @@ dashboard2_panel <- function() {
   tabPanel(
     value = "dashboard2",
     "Pupil ready reckoner",
-
-    # Define UI for application that draws a histogram
-
-    # Sidebar with a slider input for number of bins
     gov_main_layout(
-      gov_row(
-        column(
-          width = 12,
-          h1("Pupil level ready reckoner")
-        ),
-        column(
-          width = 12,
-          tabsetPanel(
-            id = "tabsetpanels2",
-            tabPanel(
-              "Progress 8",
-              fluidRow(
-                column(
-                  width = 12,
-                  h3("Select the pupil's key stage 2 scaled score for READING"),
-                  selectInput("readinginput",
-                    label = "If the pupil does not have a scaled score but has a grade 'N' or a teacher assessed grade, select 'N' or select their TAOUTCOME code and year.",
-                    choices = choicesPupil$label,
-                    selected = "Grade = N"
-                  ),
-                  h3("Select the pupil's key stage 2 scaled score for MATHEMATICS"),
-                  selectInput("mathsinput",
-                    label = "If the pupil does not have a scaled score but has a grade 'N' or a teacher assessed grade, select 'N' or select their TAOUTCOME code and year.",
-                    choices = choicesPupil$label,
-                    selected = "Grade = N"
-                  ),
-                  layout_columns(
-                    valueBoxOutput("boxavgreadmaths", width = 12),
-                    valueBoxOutput("PAGcatbox", width = 12),
-                    valueBoxOutput("PAGbox", width = 12),
-                    col_widths = c(4, 4, 4)
-                  ),
-                  layout_columns(
-                    numericInput("p8scoreeng", p("Enter the pupil's key stage 4 attainment score for their P8 English element:"), 0, min = 0, max = 18, step = 0.01),
-                    numericInput("p8scoremath", p("Enter the pupil's key stage 4 attainment score for their P8 maths element:"), 0, min = 0, max = 18, step = 0.01),
-                    col_widths = c(6, 6)
-                  ),
-                  layout_columns(
-                    numericInput("p8scoreebac", p("Enter the pupil's key stage 4 attainment score for their P8 EBacc element:"), 0, min = 0, max = 27, step = 0.01),
-                    numericInput("p8scoreopen", p("Enter the pupil's key stage 4 attainment score for their P8 open element:"), 0, min = 0, max = 27, step = 0.01),
-                    col_widths = c(6, 6)
-                  ),
+      h1("Pupil level ready reckoner"),
+      navset_tab(
+        id = "tabsetpanels2",
+        nav_panel(
+          "Progress 8",
+          card(
+            class = "border-0",
+            tags$b("Select the pupil's key stage 2 scaled scores:"),
+            layout_columns(
+              selectInput("readinginput",
+                label = tags$b("Reading"),
+                choices = choicesPupil$label,
+                selected = "Grade = N"
+              ),
+              selectInput("mathsinput",
+                label = tags$b("Mathematics"),
+                choices = choicesPupil$label,
+                selected = "Grade = N"
+              ),
+              col_widths = c(6, 6)
+            ),
+            tags$em("If the pupil does not have a scaled score but has a grade 'N' or a teacher assessed grade, select 'N' or select their TAOUTCOME code and year."),
+            layout_columns(
+              valueBoxOutput("boxavgreadmaths", width = 12),
+              valueBoxOutput("PAGcatbox", width = 12),
+              valueBoxOutput("PAGbox", width = 12),
+              col_widths = c(4, 4, 4)
+            ),
+            layout_columns(
+              numericInput("p8scoreeng", p("Enter the pupil's key stage 4 attainment score for their P8 English element:"), 0, min = 0, max = 18, step = 0.01),
+              numericInput("p8scoremath", p("Enter the pupil's key stage 4 attainment score for their P8 maths element:"), 0, min = 0, max = 18, step = 0.01),
+              col_widths = c(6, 6)
+            ),
+            layout_columns(
+              numericInput("p8scoreebac", p("Enter the pupil's key stage 4 attainment score for their P8 EBacc element:"), 0, min = 0, max = 27, step = 0.01),
+              numericInput("p8scoreopen", p("Enter the pupil's key stage 4 attainment score for their P8 open element:"), 0, min = 0, max = 27, step = 0.01),
+              col_widths = c(6, 6)
+            ),
 
-                  # numericInput("p8score", p("Enter the pupil's key stage 4 attainment score:"), sum("input.p8scoreeng"), min = 0, max = 95, step = 0.01),
-                  uiOutput("p8scoreinputbox")
-                ),
-                accordion(
-                  accordion_panel(
-                    "Progress 8 element",
-                    layout_columns(
-                      valueBoxOutput("estimatedscorebox", width = 12),
-                      valueBoxOutput("VAscorebox", width = 12),
-                      valueBoxOutput("VAscoreavbox", width = 12),
-                      col_widths = c(4, 4, 4)
-                    )
-                  ),
-                  accordion_panel(
-                    "Progress 8 - English element",
-                    layout_columns(
-                      valueBoxOutput("estimatedscoreboxeng", width = 12),
-                      valueBoxOutput("VAscoreboxeng", width = 12),
-                      valueBoxOutput("VAscoreavboxeng", width = 12),
-                      col_widths = c(4, 4, 4)
-                    )
-                  ),
-                  accordion_panel(
-                    "Progress 8 - maths element",
-                    layout_columns(
-                      valueBoxOutput("estimatedscoreboxmath", width = 12),
-                      valueBoxOutput("VAscoreboxmath", width = 12),
-                      valueBoxOutput("VAscoreavboxmath", width = 12),
-                      col_widths = c(4, 4, 4)
-                    )
-                  ),
-                  accordion_panel(
-                    "Progress 8 - EBacc element",
-                    layout_columns(
-                      valueBoxOutput("estimatedscoreboxebac", width = 12),
-                      valueBoxOutput("VAscoreboxebac", width = 12),
-                      valueBoxOutput("VAscoreavboxebac", width = 12),
-                      col_widths = c(4, 4, 4)
-                    )
-                  ),
-                  accordion_panel(
-                    "Progress 8 - open element",
-                    layout_columns(
-                      valueBoxOutput("estimatedscoreboxopen", width = 12),
-                      valueBoxOutput("VAscoreboxopen", width = 12),
-                      valueBoxOutput("VAscoreavboxopen", width = 12),
-                      col_widths = c(4, 4, 4)
-                    )
-                  )
-                ),
-                h4("Estimated against actual KS4 outcome"),
-                plotlyOutput("estvsactual")
-              )
-            )
-            #                            )
-
-            ,
-            tabPanel(
-              "EBacc subject area",
-              fluidRow(
-                column(
-                  width = 12,
-                  h3("Select the pupil's key stage 2 scaled score for READING"),
-                  selectInput("readinginputebac",
-                    label = "If the pupil does not have a scaled score but has a grade 'N' or a teacher assessed grade, select 'N' or select their TAOUTCOME code and year.",
-                    choices = choicesPupil$label,
-                    selected = "Grade = N"
-                  ),
-                  h3("Select the pupil's key stage 2 scaled score for MATHEMATICS"),
-                  selectInput("mathsinputebac",
-                    label = "If the pupil does not have a scaled score but has a grade 'N' or a teacher assessed grade, select 'N' or select their TAOUTCOME code and year.",
-                    choices = choicesPupil$label,
-                    selected = "Grade = N"
-                  ),
-                  layout_columns(
-                    valueBoxOutput("boxavgreadmathsebac", width = 12),
-                    valueBoxOutput("PAGcatboxebac", width = 12),
-                    valueBoxOutput("PAGboxebac", width = 12),
-                    col_widths = c(4, 4, 4)
-                  ),
-                  #   h3("English Baccalaureate - science subject area"),
-                  br(), gov_row(
-                    column(
-                      width = 4,
-                      numericInput("ebacscoresci", p("Enter the pupil's score in the science EBacc element:"), 0, min = 0, max = 9, step = 0.01)
-                    ),
-                    column(
-                      width = 4,
-                      numericInput("ebacscorehum", p("Enter the pupil's score in the humanities EBacc element:"), 0, min = 0, max = 9, step = 0.01)
-                    ),
-                    column(
-                      width = 4,
-                      numericInput("ebacscorelan", p("Enter the pupil's score in the languages EBacc element:"), 0, min = 0, max = 9, step = 0.01)
-                    )
-                  ),
-                  valueBoxOutput("estimatedscoreboxebacsci", width = 6),
-                  valueBoxOutput("VAscoreboxebacsci", width = 6),
-                  # valueBoxOutput("VAscoreavbox", width = 6)),
-                  #  h3("English Baccalaureate - humanities subject area"),
-                  valueBoxOutput("estimatedscoreboxebachum", width = 6),
-                  valueBoxOutput("VAscoreboxebachum", width = 6),
-                  # valueBoxOutput("VAscoreavboxeng", width = 6),
-                  #   h3("English Baccalaureate - languages subject area"),
-                  valueBoxOutput("estimatedscoreboxebaclan", width = 6),
-                  valueBoxOutput("VAscoreboxebaclan", width = 6),
-                  #     valueBoxOutput("VAscoreavboxebaclan", width = 6)),
-                  radioGroupButtons("chartbutton",
-                    label = "Choose which metric you would like to view:",
-                    choices = c("Science", "Humanities", "Languages")
-                  ),
-                  h4("Estimated against actual KS4 outcome"),
-                  conditionalPanel(
-                    "input.chartbutton == 'Science'",
-                    plotlyOutput("estvsactualebacsci")
-                  ),
-                  conditionalPanel(
-                    "input.chartbutton == 'Humanities'",
-                    plotlyOutput("estvsactualebachum")
-                  ),
-                  conditionalPanel(
-                    "input.chartbutton == 'Languages'",
-                    plotlyOutput("estvsactualebaclan")
-                  )
-                  # ,
-                  #    br(),
-                  #    br(),
-                  #    br(),
-                  #    h3("Progress 8 - EBacc element"),
-                  #    br(),
-                  #    numericInput("p8scoreebac", p("Enter the pupil's key stage 4 attainment score for their P8 EBacc element:"), 0, min = 0, max = 27, step = 0.01),
-                  #    valueBoxOutput("estimatedscoreboxebac", width = 6),
-                  #    valueBoxOutput("VAscoreboxebac", width = 6),
-                  #    valueBoxOutput("VAscoreavboxebac", width = 6),
-                  #    br(),
-                  #    br(),
-                  #    br(),
-                  #    h3("Progress 8 - open element"),
-                  #    br(),
-                  #    numericInput("p8scoreopen", p("Enter the pupil's key stage 4 attainment score for their P8 open element:"), 0, min = 0, max = 27, step = 0.01),
-                  #    valueBoxOutput("estimatedscoreboxopen", width = 6),
-                  #    valueBoxOutput("VAscoreboxopen", width = 6),
-                  #    valueBoxOutput("VAscoreavboxopen", width = 6)
+            # numericInput("p8score", p("Enter the pupil's key stage 4 attainment score:"), sum("input.p8scoreeng"), min = 0, max = 95, step = 0.01),
+            uiOutput("p8scoreinputbox"),
+            accordion(
+              accordion_panel(
+                "Progress 8 element",
+                layout_columns(
+                  valueBoxOutput("estimatedscorebox", width = 12),
+                  valueBoxOutput("VAscorebox", width = 12),
+                  valueBoxOutput("VAscoreavbox", width = 12),
+                  col_widths = c(4, 4, 4)
+                )
+              ),
+              accordion_panel(
+                "Progress 8 - English element",
+                layout_columns(
+                  valueBoxOutput("estimatedscoreboxeng", width = 12),
+                  valueBoxOutput("VAscoreboxeng", width = 12),
+                  valueBoxOutput("VAscoreavboxeng", width = 12),
+                  col_widths = c(4, 4, 4)
+                )
+              ),
+              accordion_panel(
+                "Progress 8 - maths element",
+                layout_columns(
+                  valueBoxOutput("estimatedscoreboxmath", width = 12),
+                  valueBoxOutput("VAscoreboxmath", width = 12),
+                  valueBoxOutput("VAscoreavboxmath", width = 12),
+                  col_widths = c(4, 4, 4)
+                )
+              ),
+              accordion_panel(
+                "Progress 8 - EBacc element",
+                layout_columns(
+                  valueBoxOutput("estimatedscoreboxebac", width = 12),
+                  valueBoxOutput("VAscoreboxebac", width = 12),
+                  valueBoxOutput("VAscoreavboxebac", width = 12),
+                  col_widths = c(4, 4, 4)
+                )
+              ),
+              accordion_panel(
+                "Progress 8 - open element",
+                layout_columns(
+                  valueBoxOutput("estimatedscoreboxopen", width = 12),
+                  valueBoxOutput("VAscoreboxopen", width = 12),
+                  valueBoxOutput("VAscoreavboxopen", width = 12),
+                  col_widths = c(4, 4, 4)
                 )
               )
+            ),
+            h4("Estimated against actual KS4 outcome"),
+            plotlyOutput("estvsactual")
+          )
+        ),
+        nav_panel(
+          "EBacc subject area",
+          card(
+            class = "border-0",
+            tags$b("Select the pupil's key stage 2 scaled scores:"),
+            layout_columns(
+              selectInput("readinginputebac",
+                label = tags$b("Reading"),
+                choices = choicesPupil$label,
+                selected = "Grade = N"
+              ),
+              selectInput("mathsinputebac",
+                label = tags$b("Mathematics"),
+                choices = choicesPupil$label,
+                selected = "Grade = N"
+              ),
+              col_widths = c(6, 6)
+            ),
+            tags$em("If the pupil does not have a scaled score but has a grade 'N' or a teacher assessed grade, select 'N' or select their TAOUTCOME code and year."),
+            layout_columns(
+              valueBoxOutput("boxavgreadmathsebac", width = 12),
+              valueBoxOutput("PAGcatboxebac", width = 12),
+              valueBoxOutput("PAGboxebac", width = 12),
+              col_widths = c(4, 4, 4)
+            ),
+            #   h3("English Baccalaureate - science subject area"),
+            layout_columns(
+              numericInput(
+                "ebacscoresci",
+                p("Enter the pupil's score in the science EBacc element:"),
+                0,
+                min = 0, max = 9, step = 0.01
+              ),
+              numericInput(
+                "ebacscorehum",
+                p("Enter the pupil's score in the humanities EBacc element:"),
+                0,
+                min = 0, max = 9, step = 0.01
+              ),
+              numericInput(
+                "ebacscorelan",
+                p("Enter the pupil's score in the languages EBacc element:"),
+                0,
+                min = 0, max = 9, step = 0.01
+              ),
+              col_widths = c(4, 4, 4)
+            ),
+            valueBoxOutput("estimatedscoreboxebacsci", width = 6),
+            valueBoxOutput("VAscoreboxebacsci", width = 6),
+            # valueBoxOutput("VAscoreavbox", width = 6)),
+            #  h3("English Baccalaureate - humanities subject area"),
+            valueBoxOutput("estimatedscoreboxebachum", width = 6),
+            valueBoxOutput("VAscoreboxebachum", width = 6),
+            # valueBoxOutput("VAscoreavboxeng", width = 6),
+            #   h3("English Baccalaureate - languages subject area"),
+            valueBoxOutput("estimatedscoreboxebaclan", width = 6),
+            valueBoxOutput("VAscoreboxebaclan", width = 6),
+            #     valueBoxOutput("VAscoreavboxebaclan", width = 6)),
+            radioGroupButtons("chartbutton",
+              label = "Choose which metric you would like to view:",
+              choices = c("Science", "Humanities", "Languages")
+            ),
+            h4("Estimated against actual KS4 outcome"),
+            conditionalPanel(
+              "input.chartbutton == 'Science'",
+              plotlyOutput("estvsactualebacsci")
+            ),
+            conditionalPanel(
+              "input.chartbutton == 'Humanities'",
+              plotlyOutput("estvsactualebachum")
+            ),
+            conditionalPanel(
+              "input.chartbutton == 'Languages'",
+              plotlyOutput("estvsactualebaclan")
             )
+            # ,
+            #    br(),
+            #    br(),
+            #    br(),
+            #    h3("Progress 8 - EBacc element"),
+            #    br(),
+            #    numericInput("p8scoreebac", p("Enter the pupil's key stage 4 attainment score for their P8 EBacc element:"), 0, min = 0, max = 27, step = 0.01),
+            #    valueBoxOutput("estimatedscoreboxebac", width = 6),
+            #    valueBoxOutput("VAscoreboxebac", width = 6),
+            #    valueBoxOutput("VAscoreavboxebac", width = 6),
+            #    br(),
+            #    br(),
+            #    br(),
+            #    h3("Progress 8 - open element"),
+            #    br(),
+            #    numericInput("p8scoreopen", p("Enter the pupil's key stage 4 attainment score for their P8 open element:"), 0, min = 0, max = 27, step = 0.01),
+            #    valueBoxOutput("estimatedscoreboxopen", width = 6),
+            #    valueBoxOutput("VAscoreboxopen", width = 6),
+            #    valueBoxOutput("VAscoreavboxopen", width = 6)
           )
         )
       )
