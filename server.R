1f99374e182275ad01cedf5a5b3257873bedda88
Rich Bielby <richard.bielby@education.gov.uk>
2023-10-19 10:55:54 +0100

    Sidestepped error/warning associated with KS4 outcome chart

--- server.R
+++ server.R
@@ -322,12 +322,21 @@ server <- function(input, output, session) {
   })
 
   output$estvsactual <- renderPlotly({
-    estvsactual <- ggplot(df, aes(x = x, y = y)) +
-      geom_line() +
-      geom_point(x = as.numeric(reactiveestimated()), y = input$p8score, size = 2, colour = "#FF007F") +
+    p8estimated <- as.numeric(reactiveestimated())
+    p8score <- ifelse(is.null(input$p8score), 0, input$p8score)
+    data <- data.frame(p8estimated = c(p8estimated), p8actual = c(p8score))
+    estvsactual <- ggplot(
+      data = data,
+      aes(x = p8estimated, y = p8actual)
+    ) +
+      geom_point(colour = "#12436D") +
+      geom_abline(intercept = 0, slope = 1, color = "#28A197", linetype = "dashed") +
       # ggtitle("Estimated against actual KS4 outcome") +
       xlab("Estimated KS4 outcome") +
       ylab("Actual KS4 outcome") +
+      xlim(0, 90) +
+      ylim(0, 90) +
+      theme_classic() +
       theme(
         plot.title = element_text(color = "black", size = 14, face = "bold"),
         axis.title.x = element_text(color = "black", size = 10, face = "plain"),
